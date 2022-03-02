da0dfb80384ee81a86e7466a861498a4e80473e5
Vishnu Suresh Perumbavoor <63797249+vishnusureshperumbavoor@users.noreply.github.com>
2022-03-02 12:04:27 +0530

    mysql select query inserted

--- connect.php
+++ connect.php
@@ -1,14 +1,18 @@
 <?php
 $localhost = "localhost";
 $user = "root";
-$password = "";
+$pass = "";
 $database = "s3ct";
-$connect = mysqli_connect($localhost,$user,$password,$database);
+$connect = mysqli_connect($localhost,$user,$pass,$database);
 if($_POST['submit']){
     $fname = $_POST['firstname'];
     $lname = $_POST['lastname'];
-    $pass = $_POST['password'];
-    mysqli_query($connect,"INSERT INTO employee (firstname,lastname,secretkey) VALUES ('$fname','$lname','$pass')");   
-    echo "<h1 style='text:bold; margin-top:200px; text-align:center; text-transform:uppercase; font-size:350% ' > 
-    welcome $fname $lname </h1>";
+    $pword = $_POST['password'];
+    mysqli_query($connect,"INSERT INTO employee (firstname,lastname,password) VALUES ('$fname','$lname','$pword')");   
+    $sql = mysqli_query($connect,"SELECT * FROM employee");
+    echo "<h1 style='text:bold; margin-top:1px; text-align:center; text-transform:uppercase; font-size:100% ' > EMPLOYEE NAMES </h1><br>";
+    while ($row = $sql->fetch_assoc()){
+        echo "<h2 style='text:bold; margin-top:1px; text-align:center; text-transform:uppercase; font-size:100% ' > 
+        " . $row["slno"] . " . " . $row["firstname"] . " " . $row["lastname"] . "</h2><br>";
+    }
 }
